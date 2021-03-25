b1bc97ff10c29ed30f18e66344fe863da97291b0
J. Rodrigues <80002311+j-rodrigues-stack@users.noreply.github.com>
2021-03-25 16:03:53 -0300

    adding image carousel

---
+++ js/owl/main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    autoWidth:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
